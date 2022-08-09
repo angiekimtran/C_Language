53515f9fcf68f3cbd70b91795f7ed09c668ae5ba
angiekimtran <tranangiekim@gmail.com>
2022-08-08 19:04:30 -0600

    Committing Integer Project

---
+++ IntegerVariablesAndOperations/main.c
@@ -0,0 +1,34 @@
+/*
+* File:   main.c
+* Author: Angie Tran
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// constants for Problem 2
+#define OLD_AGE_YEAR 1962
+#define CURRENT_YEAR 2019
+
+/*
+* Exercise 2 solution
+*/
+int main(int argc, char** argv)
+{
+	// Problem 1: Output age
+	int age = 22;
+	printf("My age: %d\n", age);
+
+	// Problem 2: Output old age
+	int oldAge = CURRENT_YEAR - OLD_AGE_YEAR;
+	printf("Old age: %d\n", oldAge);
+
+	// Problem 3: Output difference in ages (with new variable)
+	int ageDifference = oldAge - age;
+	printf("Old age is %d years older than me\n", ageDifference);
+
+	// Problem 3: Output difference in ages (no variable)
+	//printf("Old age is %d years older than me\n", oldAge - age);
+
+	return (EXIT_SUCCESS);
+}
