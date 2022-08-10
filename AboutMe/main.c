2e7c3cf74ea85afa3e3a3f5257d090736ef09791
angiekimtran <tranangiekim@gmail.com>
2022-08-09 21:36:36 -0600

    Added About Me Program

---
+++ AboutMe/main.c
@@ -0,0 +1,23 @@
+/*
+ * File:   main.c
+ * Author: <Angie Tran>
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+ /*
+  * About Me: Angie Tran
+  */
+int main(int argc, char** argv)
+
+{
+	//Information About Me 
+	int Age = 22;
+	printf("My Name: Angie Tran\n");
+	printf("My Age:%d\n", Age);
+	printf("My Hobbies: Video Games, TV Shows, and Singing");
+	return (EXIT_SUCCESS);
+}
